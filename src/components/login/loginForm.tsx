b6ae5726d44e940e9c5708dda27a7d6c3f16648c
ksj <tankboya@naver.com>
2023-03-30 11:24:27 +0900

    로그인 페이지 화면 구현

--- src/components/login/loginForm.tsx
+++ src/components/login/loginForm.tsx
@@ -1,11 +1,55 @@
+// 로그인 폼 컴포넌트
 import React from 'react';
+import styled from 'styled-components';
 
-function LoginForm(
+const LoginFormWrapper = styled.div`
+  .title {
+    margin-bottom: 20px;
+    font-size: 20px;
+    font-weight: bold;
+    color: #127369;
+  }
 
-) {
+  form input {
+    border: none;
+    border-bottom: 1px solid #10403B;
+    width: 100%;
+    outline: none;
+    font-size: 15px;
+    margin-bottom: 20px;
+    background-color: #BFBFBF;
+    color: #000;
+  }
+
+  form button {
+    background-color: #8AA6A3;
+    border: none;
+    color: #eee;
+    border-radius: 4px;
+    padding: 5px 10px;
+    font-weight: bold;
+    font-size: 15px;
+    outline: none;
+    cursor: pointer;
+    width: 100%;
+
+    &:hover {
+      background-color: #127369;
+    }
+  }
+`;
+
+const LoginForm: React.FC = ({}) => {
   return (
-    <div></div>
-  );
+    <LoginFormWrapper>
+      <div className='title'>Login</div>
+      <form>
+        <input placeholder='ID' name="userId"/>
+        <input placeholder='Password' name="password" type="password"/>
+        <button>Login</button>
+      </form>
+    </LoginFormWrapper>
+  )
 }
 
 export default LoginForm;
--- src/components/login/loginTemplate.tsx
+++ src/components/login/loginTemplate.tsx
@@ -1,9 +1,67 @@
-import React from 'react';
+// 로그인 폼을 감싸는 템플릿 컴포넌트
+// 여기서 상태를 관리함
+import React, {useState, useCallback, useEffect} from 'react'
+import styled from 'styled-components'
+import LoginForm from './LoginForm';
 
-function LoginTemplate() {
+const LoginTemplateWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #eee;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginBox = styled.div`
+  background-color: #BFBFBF;
+  width: 450px;
+  border-radius: 10px;
+  padding: 20px 70px 50px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+
+  .logo {
+    display: block;
+    text-align: center;
+    font-weight: bold;
+    font-size: 25px;
+    letter-spacing: 2px;
+    margin-bottom: 20px;
+    color: #127369;
+  }
+`
+
+const LoginTemplate: React.FC = () => {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  // input태그에 들어온 값을 userId, password에 설정
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+    setPassword(e.target.value);
+  }, []);
+
+  // 로그인 버튼을 눌렀을 때, 저장된 userId, password 값을 서버로 보냄
+  const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    // input에 들어간 값은 다시 초기화
+    setUserId('');
+    setPassword('');
+    e.preventDefault();
+  }, []);
+
   return (
-    <div></div>
-  );
+    <LoginTemplateWrapper>
+      <LoginBox>
+        <div className='logo'>G. Ware 2023</div>
+        <LoginForm/>
+      </LoginBox>
+    </LoginTemplateWrapper>
+  )
 }
 
 export default LoginTemplate;
