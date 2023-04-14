c6e866430e2dcae544154fa6c9522cf3807ba303
ijaesin <ijaesinii@gmail.com>
2023-04-14 15:09:52 +0900

    관리자 페이지 화면 개발

--- src/components/Main/MyProfileLogo.tsx
+++ src/components/Main/MyProfileLogo.tsx
@@ -7,12 +7,12 @@ export default function MyProfileLogo({ moveToMain }: MyProfileProps) {
     <div>
       <button
         onClick={moveToMain}
-        className="inline-block text-green3 font-medium italic underline hover:no-underline px-3 py-2 hover:rounded-md hover:text-gray1 hover:bg-green3"
+        className="inline-block px-3 py-2 italic font-medium underline text-green3 hover:no-underline hover:rounded-md hover:text-gray1 hover:bg-green3"
       >
         <span className="hidden">메인으로 돌아가는 버튼</span>
         Go to Main
       </button>
-      <div className="text-gray1 inline-block text-xl font-bold ml-5">
+      <div className="inline-block ml-5 text-xl font-bold text-gray1">
         My Profile
       </div>
     </div>
