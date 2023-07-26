a069254267a1ad8c167933ec5bcec87736f7d491
ijaesin <ijaesinii@gmail.com>
2023-07-26 10:59:07 +0900

    PostPage에 userName, viewCount, createDatetime 추가

--- src/components/post/read/PostPage.tsx
+++ src/components/post/read/PostPage.tsx
@@ -1,18 +1,39 @@
 import { sample_posts } from "../../../sample";
 import { getPost } from "../../../api/post/post";
-import { useEffect, useState } from "react";
+import Image from "next/image";
 
 interface PostPageProps {
   postId: number;
 }
 
 export default function PostPage({ postId }: PostPageProps) {
-  const [currentPost, setCurrentPost] = useState(sample_posts[postId]);
+  const currentPost = sample_posts[postId];
 
   return (
-    <div>
-      <div>{currentPost.title}</div>
-      <div>{currentPost.content}</div>
+    <div className="flex flex-col px-20">
+      <div className="mb-10">
+        <div className="mb-1 text-3xl font-bold text-green2">
+          {currentPost.title}
+        </div>
+        <div className="flex place-content-between text-green1">
+          <div className="flex">
+            {currentPost.userName} &#183;{" "}
+            <Image
+              className="w-auto h-4 mt-1 ml-2 mr-1"
+              src="/view.png"
+              alt="view-count"
+              width={10}
+              height={10}
+            />
+            {currentPost.viewCount}
+          </div>
+          <div>{currentPost.createDatetime}</div>
+        </div>
+      </div>
+      <div className="text-lg text-green3">{currentPost.content}</div>
+      <div className="mx-auto mt-20 text-green3">
+        {currentPost.recommendationCount}
+      </div>
     </div>
   );
 }
--- src/sample/index.ts
+++ src/sample/index.ts
@@ -6,5 +6,10 @@ function makeObj(num: number) {
     title: `Post ${num + 1}`,
     content:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cupiditate, saepe sunt distinctio porro eligendi libero aliquid doloribus necessitatibus dolore similique nulla placeat esse consectetur laudantium in quam fugit? Optio.",
+    userId: num,
+    userName: `User ${num}`,
+    createDatetime: "2023-07-10 10:42:30",
+    viewCount: 0,
+    recommendationCount: 0,
   };
 }
