e8a47a5ddf37f4613510ebcb73630ec7b1dfe447
ijaesin <ijaesinii@gmail.com>
2023-08-03 16:40:36 +0900

    PostForm 컴포넌트 UI 수정

--- src/components/post/write/PostForm.tsx
+++ src/components/post/write/PostForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import { createPost } from "../../../api/post/post";
 import { useRouter } from "next/router";
 
@@ -35,8 +35,8 @@ export default function () {
   }, []);
 
   return (
-    <div className="flex justify-center mt-10">
-      <div className="w-full p-10 mx-20 bg-gray1 rounded-3xl min-w-[760px]">
+    <div className="flex justify-center">
+      <div className="w-full p-10 mx-20  rounded-3xl min-w-[760px]">
         <div className="mb-10 text-3xl font-bold text-center text-green2">
           New Post
         </div>
@@ -44,28 +44,27 @@ export default function () {
           <input
             type="text"
             placeholder="Title"
-            className="px-3 py-1 mb-5 text-lg rounded-xl"
+            className="px-3 py-1 mb-5 text-lg rounded-xl bg-gray1"
             name="title"
             value={title}
             onChange={onChangeTitle}
           />
           <textarea
             placeholder="Content"
-            className="mb-5 py-1 min-h-[400px] rounded-xl px-3 text-lg"
-            name="content"
+            className="mb-5 py-1 min-h-[400px] rounded-xl px-3 text-lg bg-gray1"
             value={content}
             onChange={onChangeContent}
           />
 
           <button
             type="submit"
-            className="py-2 mb-5 text-lg font-bold rounded-lg bg-green2 text-gray1 hover:bg-green3"
+            className="py-2 mb-5 text-lg font-bold text-white rounded-lg bg-green2 hover:bg-green3"
           >
             Submit
           </button>
           <button
             type="button"
-            className="py-2 text-lg font-bold rounded-lg bg-gray2 text-gray1 hover:bg-red-500"
+            className="py-2 text-lg font-bold text-white rounded-lg bg-gray2 hover:bg-red-500"
             onClick={cancelToPost}
           >
             Cancel
