3a3eec4e5b9990b6daa93d737c0be486953e0958
ijaesin <ijaesinii@gmail.com>
2023-08-07 18:35:39 +0900

    PostForm에 file input 추가

--- src/components/post/write/PostForm.tsx
+++ src/components/post/write/PostForm.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { createPost } from "../../../api/post/post";
 import { useRouter } from "next/router";
+import useFileSize from "../../../hooks/useFileSize";
 
 export default function () {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const router = useRouter();
+  const [curFiles, setCurFiles] = useState<string[]>([]);
 
   const onChangeTitle = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,6 +36,19 @@ export default function () {
     // });
   }, []);
 
+  const onChangeFiles = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const fileList = Array.from(e.target.files);
+        const fileNames = fileList.map(
+          (file) => `${file.name} (${useFileSize(file.size)})`
+        );
+        setCurFiles([...curFiles, ...fileNames]);
+      }
+    },
+    [curFiles]
+  );
+
   return (
     <div className="flex justify-center">
       <div className="w-full p-10 mx-20  rounded-3xl min-w-[760px]">
@@ -48,13 +63,49 @@ export default function () {
             name="title"
             value={title}
             onChange={onChangeTitle}
+            required
           />
+
           <textarea
             placeholder="Content"
             className="mb-5 py-1 min-h-[400px] rounded-xl px-3 text-lg bg-gray1"
             value={content}
             onChange={onChangeContent}
+            required
           />
+
+          <div className="mb-5 overflow-hidden rounded-xl">
+            <div className="py-1 text-lg font-bold text-center text-white bg-gray3">
+              Upload File
+            </div>
+            <div className="p-2 bg-gray1">
+              <div className="mt-2 ml-3">
+                <label
+                  htmlFor="file_uploads"
+                  className="px-2 py-1 font-bold text-white rounded-lg cursor-pointer bg-green2 hover:bg-green3"
+                >
+                  Choose file to upload
+                </label>
+                <input
+                  type="file"
+                  className="hidden"
+                  id="file_uploads"
+                  name="file"
+                  multiple
+                  onChange={onChangeFiles}
+                />
+              </div>
+              <div className="h-20 px-2 py-1 m-3 overflow-scroll bg-white rounded-lg">
+                {curFiles.length === 0 ? (
+                  <p className="text-gray2">
+                    No files currently selected for upload.
+                  </p>
+                ) : (
+                  curFiles.map((file, i) => <p key={i}>{file}</p>)
+                )}
+              </div>
+            </div>
+          </div>
 
           <button
             type="submit"
